e57cfebc91905e6334520b5e4816315035765d02
nicolas.cian <nicolas.cian@e7bd4e72-cdc3-51db-c69a-d1f7cd60790b>
2010-09-21 14:52:54 +0000

    Getter y setter en cliente carpeta Test

--- trunk/src/Model/Cliente.java
+++ trunk/src/Model/Cliente.java
@@ -1,9 +1,43 @@
 package Model;
 
 public class Cliente {
+	//Atributos
 	private int telefono;
 	private String direccion;
 	private Historial historial;//EstadisticasDeUso
-	private String nombreUsuario;
+	private String nombreUsuario; //para el login
+	private String nombreCompleto;
+	
+	//metodos getter y setters
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public Historial getHistorial() {
+		return historial;
+	}
+	public void setHistorial(Historial historial) {
+		this.historial = historial;
+	}
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+	public String getNombreCompleto() {
+		return nombreCompleto;
+	}
+	public void setNombreCompleto(String nombreCompleto) {
+		this.nombreCompleto = nombreCompleto;
+	}
 	
 }
