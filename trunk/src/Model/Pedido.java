c6088f4ed1d9bdc98016eeb00678927758134b58
nicolas.cian@gmail.com <nicolas.cian@gmail.com@e7bd4e72-cdc3-51db-c69a-d1f7cd60790b>
2011-06-30 19:50:48 +0000

    Actualizacion de las clases Modelos

--- trunk/src/Model/Pedido.java
+++ trunk/src/Model/Pedido.java
@@ -4,50 +4,29 @@ import java.util.ArrayList;
 import java.util.Date;
 
 /**
- * Un Pedido es un conjunto de Items, estos items pueden pertenecer a distintos proveedores, por ello
- * el pedido es enviado al dispatcher que toma el pedido, lo filtra por proveedor y lo reparte a los proveedores.
+ * 
  */
 public class Pedido {
-//un cliente tiene varios pedidos
+
+	//Atributos
+	private ArrayList<Comida> comidas;
+	private ArrayList<Cliente> clientes;
 	
-	/**
-	 * NEFASTA IMPLEMENTACION CON STRINGS
-	 * TODO: ARREGLAR
-	 */
-	
-	//This code sucks, you know it and I know it. 
-	//Move on and call me an idiot later.
-	private boolean confirmado;//confirmado o no confirmado (se podria decir que esta en espera) -> true or false
-	private boolean cancelado;// cancelado o "confirmado o en espera"
-	private Date fechaCreacion; //Fecha en que el pedido fue realizado
-	private Date fechaConfirmado; //Fecha en que el pedido fue confirmado
-	private Date fechaEnvio; //En caso de realizar un pedido a fecha postergada
-	private ArrayList<Item> listado;
+	private Date fecha_hora; //TIMESTAMP en que el pedido fue realizado
+	private int cantidad;
+
 
 	//metodos
 	/**
 	 * Constructor
-	 * por defecto un Pedido no esta confirmado, esta pendiente
 	 */
 	public Pedido() {
-		confirmado = false;
-		cancelado = false;
+
 	}
+
 	
-	public boolean is_confirmado() {
-		return confirmado;
-	}
 	
-	public void confirmar() {
-		confirmado = true;
-	}
-	
-	//un pedido se puede cancelar solo si no ha sido confirmado
-	//en otras palabras, solo se puede cancelar si estaba en espera
-	public void cancelar() throws imposibleCancelarPedidoException {
-		if(!confirmado) cancelado = true;
-		else throw new imposibleCancelarPedidoException(this);
-	}
+	//metodos getter y setters
 	
 	/**
 	* For the brave souls who get this far: You are the chosen ones,
@@ -57,17 +36,5 @@ public class Pedido {
 	* never gonna run around and desert you. Never gonna make you cry,
 	* never gonna say goodbye. Never gonna tell a lie and hurt you.
 	*/
-	
-	public void agregarItem(Item item){
-		listado.add(item);
-	}
-	
-	public void quitarItem(Item item){
-		//////////buscar y eliminar//////////////
-	}
-	
-	public ArrayList<Item> getItems() {
-		return listado;
-	}
 	
 }
--- trunk/src/Model/imposibleCancelarPedidoException.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-public class imposibleCancelarPedidoException extends Exception {
-
-	public imposibleCancelarPedidoException(Pedido pedido) {
-		// TODO: mostrar los datos del pedido q causo la excepcion
-	}
-
-}
